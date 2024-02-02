e483ef997a8b50e648f96feafdbfee928b9a312f
gudulu <rrnayak165@gmail.com>
2024-02-02 14:09:32 +0530

    poject create

---
+++ src/main/java/com/project/idareu/IdareuApplication.java
@@ -0,0 +1,13 @@
+package com.project.idareu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdareuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdareuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/idareu/IdareuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.idareu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdareuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
